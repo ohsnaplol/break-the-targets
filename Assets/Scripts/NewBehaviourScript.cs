6019555b1fddbc21675122aa2da092222ff47fd3
Marcello <emyb@bellsouth.net>
2023-03-19 16:50:03 -0400

    dont know why this isnt working
    Very long description

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        debug.log('start :>');
     }
 
     // Update is called once per frame
