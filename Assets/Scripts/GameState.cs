4bb2104a9d52391f058628603d81afd1595bdcfb
tlneondo <tlneondo@gmail.com>
2023-05-05 11:21:34 -0400

    Added Gamestate with bad syntax

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : ScriptableObject{
+
+    int targetTotal;
+    int targetsDestroyed;
+
+}
